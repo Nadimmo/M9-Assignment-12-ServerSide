d3918970ec02c1fd2924555c38abc5009e8e84a7
nadimmo <nadimmostafa334@gmail.com>
2024-11-17 21:52:48 +0600

    create display show in survey api

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(
 );
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rrkijcq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,6 +33,18 @@ async function run() {
     app.post('/surverys/create', async(req,res)=>{
         const surverys = req.body
         const result = await CollectionOfCreateSurverys.insertOne(surverys)
+        res.send(result)
+    })
+
+    app.get('/surverys', async(req,res)=>{
+        const result = await CollectionOfCreateSurverys.find().toArray()
+        res.send(result)
+    })
+
+    app.get('/surverys/:id', async(req,res)=>{
+        const surveyId = req.params.id
+        const filter = {_id: new ObjectId(surveyId)}
+        const result = await CollectionOfCreateSurverys.findOne(filter)
         res.send(result)
     })
 
